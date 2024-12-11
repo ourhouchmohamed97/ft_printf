ec3b37fc3e93365fc592a2e397f119a123ab0ab3
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2024-12-11 17:32:57 +0100

    printf

--- ft_putchar.c
+++ ft_putchar.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#includ "ft_printf.h"
 
 void	ft_putchar(char c, int *counter)
 {
